cb7988c8a55e00504ecfdfb264936c660e0ee03f
Keith-Rich <keithkuboka@gmail.com>
2023-03-09 22:54:17 +0300

    Changed to pin.py

---
+++ pin.py
@@ -0,0 +1,75 @@
+import random
+number = int(input("Input your 4-digit-pin:"))
+guess = 0
+while(guess != number):
+    guess = random.randint(0,9999)
+    print(guess)
+print("Your pin is " + str(number))
+
+
+
+# import random
+# number = int(input("Input your 4 digit-pin:"))
+# guess = 0
+# while(guess != number):
+#     guess = random.randint(0,9999)
+# print("Your pin is " + str(number))
+
+
+
+# import random
+# number = int(input("Weka pin ya nambari 5:"))
+# guess = 0
+# while(guess != number):
+#     guess = random.randint(0,99999)
+#     print(guess)
+# print("Pin yako ni " + str(number))
+
+
+
+# import random
+# number = int(input("6 figure Pin required:"))
+# guess = 0
+# while(guess != number):
+#     guess = random.randint(0,999999)
+#     print(guess)
+# print("Pin is " + str(number))
+
+
+
+# import random
+# number = int(input("Input any 7 digit pin:"))
+# guess = 0
+# while(guess != number):
+#     guess = random.randint(0,9999999)
+# print("Pin is " + str(number))
+
+
+
+# import random
+# number = int(input("Input 8 figure pin:"))
+# guess = 0
+# while(guess != number):
+#     guess = random.randint(0,99999999)
+# print("Pin is " + str(number))
+
+
+
+# import random
+# number = int(input("9 figure pin recommended"))
+# guess = 0
+# while(guess != number):
+#     guess = random.randint(0,999999999)
+# print("The pin is "+ str(number))
+
+
+
+# import random
+# number = int(input("A 10 digit pin is allowed:"))
+# guess = 0
+# while(guess != number):
+#     guess = random.randint(0,9999999999)
+# print("The found pin is " + str(number))
+
+
+
